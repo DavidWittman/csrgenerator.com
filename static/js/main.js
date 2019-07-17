26cda17fd1dff6a8810e78ed5607f1a726a718be
Emmanuel Sackey <esackey15@alustudent.com>
2019-07-17 15:45:15 +0400

    Step 1 of form complete
    Added dynamic generation of NIC handles and filling of appropriate fields.

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,7 @@
+
+
 $(function() {
+
     $(".close").on("click", function() {
         $(this).closest(".modal").removeClass("active");
     });
@@ -13,43 +16,90 @@ $(function() {
 
     $("#csr").on("click focus", function() { this.select() } );
 
-    let endpoint = "https://rdap.afrinic.net/rdap/entity/"
-//var orgId = document.querySelector("#org-id")
-    let orgId = $("#org-id")
+    let endpoint = "https://rdap.afrinic.net/rdap/entity/";
+    let orgId = $("#org-id");
+    let nicIds;
 
     orgId.on("change", function () {
-        let data =  $.getJSON(endpoint + orgId.val(),function(data) {
-            console.log(endpoint+orgId.val())
-            let nicIds = data.entities;
+         $.getJSON(endpoint + orgId.val(),function(data) {
+            console.log(endpoint+orgId.val());
+            console.info(data.entities[0]["vcardArray"][1][5][3]);
+             nicIds = data.entities;
             let select = document.querySelector("select");
+
+
             for (let nicId in nicIds) {
                 let option = document.createElement("option");
-                let textValue = nicIds[nicId].handle
+                console.log(nicIds[nicId]);
+                let textValue = nicIds[nicId].handle;
                 option.value = textValue;
                 option.innerText = textValue;
                 select.append(option);
             }
 
         }).fail(function () {
-            alert("Enter a valid Org Handle")
+            alert("Enter a valid Org Handle");
         })
 
-    })
+    });
 
-    let nicHandle = $("#nic-id")
+    let nicHandle = $("#nic-id");
 
-    nicHandle.on("change",function () {
+    nicHandle.on("change",showForm)
+
+
+
+    function showForm() {
+        fillForm(nicIds)
         let csrForm = $("#csr-form");
 
         if(csrForm.css("display") === "none"){
             csrForm.css("display", "block");
         }
+    }
 
-    })
+    function fillForm(nicIds) {
+        let nicId = $("#nic-id").val();
+        let address = [];
+        let roles =[];
+        let orgUnit = "";
 
+
+        for (let counter in nicIds){
+            if(nicIds[counter].handle === nicId){
+                let selectedNic = nicIds[counter];
+                roles = selectedNic.roles;
+                address = selectedNic["vcardArray"][1][5][3];
+                console.log(address);
+            }
+        }
+
+        if(roles.length > 1){
+            for(let i in roles){
+                if(i < roles.length -1){
+                    orgUnit += roles[i] + ",";
+                }
+                else {
+                    orgUnit += roles[i]
+                }
+            }
+
+        }
+
+        else {
+            orgUnit = roles[0];
+        }
+
+        //$("#country").val(address[2]);
+        //$("#locality").val(address[1]);
+        $("#org-unit").val(orgUnit);
+        $("#common-name").val(nicId);
+        $("#org").val($("#org-id").val())
+    }
 
 
 });
+
 
 
 
