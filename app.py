eb820e33cce193f2edad72b48ca6dbcf4d784cef
Emmanuel Sackey <esackey15@alustudent.com>
2019-07-23 22:51:26 +0400

    working Chrome and Firefox driver configuration

--- app.py
+++ app.py
@@ -2,16 +2,17 @@
 
 from __future__ import absolute_import
 import os
-
+import logging
 from flask import Flask, request, Response, render_template
-
+import OpenSSL.crypto as crypt
 from csr import CsrGenerator
-
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
 app = Flask(__name__)
+path = os.path.dirname(os.path.abspath(__file__))
+logging.basicConfig(filename=path + '/logs/enrolment.log', format='%(asctime)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
 @app.route('/')
@@ -27,20 +28,22 @@ def security():
 @app.route('/generate', methods=['POST'])
 def generate_csr():
     csr = CsrGenerator(request.form)
-    key_size = int(request.form['keySize'])
     response = b'\n'.join([csr.csr, csr.private_key])
     nic_id = request.form['CN']
     password = request.form['password']
-    csr_pem = response[:key_size]
     generate_pem(nic_id, password, csr.csr)
+    generate_p12(nic_id, password, csr.private_key)
     return Response(response, mimetype='text/plain')
 
 
 def generate_pem(nic_id, password, csr):
-    opts = Options()
-    opts.binary_location = '/home/minty/Documents/GitHub/bpki-enrolment-gui/chromedriver'
+    opts = webdriver.ChromeOptions()
+    opts.add_argument('--no-sandbox')
     opts.add_argument('--headless')
-    browser = webdriver.Chrome('/home/minty/Documents/GitHub/bpki-enrolment-gui/chromedriver')
+    opts.add_argument('--disable-dev-shm-usage')
+    log_path = path + '/logs/chromedriver.log'
+
+    browser = webdriver.Chrome(path + '/chromedriver', options=opts, service_args=['--verbose', '--log-path=' + log_path])
     browser.get('https://externalra.afrinic.net/externalra-gui/facelet/enroll-csrcert.xhtml')
 
     pem_checkbox = browser.find_element_by_id('form:j_id71:_1')
@@ -58,8 +61,40 @@ def generate_pem(nic_id, password, csr):
     submit = browser.find_element_by_id('form:j_id77')
     submit.click()
 
-   # browser.close()
-    #browser.quit()
+    # browser.close()
+    # browser.quit()
+
+
+def generate_p12(nic_id, password, private_key):
+    working_dir = os.getcwd()
+    download_path = working_dir + '/Downloads'
+    pem_file = download_path + '/' + nic_id + '.pem'
+    output = download_path + '/' + nic_id + '.p12'
+
+    try:
+        with open(pem_file, 'rb') as pem_file:
+            pem_buffer = pem_file.read()
+            pem = crypt.load_certificate(crypt.FILETYPE_PEM, pem_buffer)
+
+    except IOError as error:
+        print('Could not read pem file. Make sure file exists and you have the right permission. ', error)
+
+    try:
+        private_key = crypt.load_privatekey(crypt.FILETYPE_PEM, private_key)
+        pfx = crypt.PKCS12Type()
+        pfx.set_privatekey(private_key)
+        pfx.set_certificate(pem)
+        pfx_data = pfx.export(password)
+
+        try:
+            with open(output, 'wb') as p12_file:
+                p12_file.write(pfx_data)
+
+        except IOError as error:
+            print('Unable to write p12 file ', error)
+
+    except crypt.Error as error:
+        print('An unexpected error occurred while generating p12 file ', error)
 
 
 if __name__ == '__main__':
--- static/js/main.js
+++ static/js/main.js
@@ -22,35 +22,45 @@ $(function() {
     let nicIds;
 
     orgId.on("change", function () {
-         $.getJSON(endpoint + orgId.val(),function(data) {
-            console.log(endpoint+orgId.val());
-            console.info(data.entities[0]["vcardArray"][1][5][3]);
-             nicIds = data.entities;
-            let select = document.querySelector("select");
+        /*   $.getJSON(endpoint + orgId.val(),function(data) {
+              console.log(endpoint+orgId.val());
+              console.info(data.entities[0]["vcardArray"][1][5][3]);
+               nicIds = data.entities;*/
+              let select = document.querySelector("select");
 
 
-            for (let nicId in nicIds) {
-                let option = document.createElement("option");
-                console.log(nicIds[nicId]);
-                let textValue = nicIds[nicId].handle;
-                option.value = textValue;
-                option.innerText = textValue;
-                select.append(option);
-            }
+        nicIds = ["IS001 - AFRINIC","ATU1-AFRINIC"]
+        console.log(nicIds);
+        for (let nicId in nicIds) {
+            console.log(nicId);
+            let option = document.createElement("option");
+            let textValue = nicIds[nicId];
+            option.value = textValue;
+            option.innerText = textValue;
+            select.append(option);
 
-        }).fail(function () {
-            alert("Enter a valid Org Handle");
-        })
+       /*     let option = document.createElement("option");
+            console.log(nicIds[nicId]);
+            let textValue = nicIds[nicId].handle;
+            option.value = textValue;
+            option.innerText = textValue;
+            select.append(option); */
+        }
 
-    });
+        /*
+                }).fail(function () {
+                    alert("Enter a valid Org Handle");
+                })
 
-    let nicHandle = $("#nic-id");
+            });
 
-    nicHandle.on("change",showForm);
+            let nicHandle = $("#nic-id");
+
+            nicHandle.on("change",showForm);
+        */
 
 
-
-    function showForm() {
+        function showForm() {
 
             fillForm(nicIds);
             let csrForm = $("#csr-form");
@@ -60,54 +70,46 @@ $(function() {
             }
 
 
-    }
-
-    function fillForm(nicIds) {
-        let nicId = $("#nic-id").val();
-        let address = [];
-        let roles =[];
-        let orgUnit = "";
-
-
-        for (let counter in nicIds){
-            if(nicIds[counter].handle === nicId){
-                let selectedNic = nicIds[counter];
-                roles = selectedNic.roles;
-                address = selectedNic["vcardArray"][1][5][3];
-                //console.log(address);
-            }
         }
 
-        if(roles.length > 1){
+        function fillForm(nicIds) {
+            let nicId = $("#nic-id").val();
+            let address = [];
+            let roles = [];
+            let orgUnit = "";
 
-            for(let i in roles){
 
-                if(i < roles.length -1){
-                    orgUnit += roles[i] + ",";
-                }
-                else {
-                    orgUnit += roles[i];
+            for (let counter in nicIds) {
+                if (nicIds[counter].handle === nicId) {
+                    let selectedNic = nicIds[counter];
+                    roles = selectedNic.roles;
+                    address = selectedNic["vcardArray"][1][5][3];
+                    //console.log(address);
                 }
             }
 
+
+            if (roles.length > 1) {
+
+                for (let i in roles) {
+
+                    if (i < roles.length - 1) {
+                        orgUnit += roles[i] + ",";
+                    } else {
+                        orgUnit += roles[i];
+                    }
+                }
+
+            } else {
+                orgUnit = roles[0];
+            }
+
+            //$("#country").val(address[2]);
+            //$("#locality").val(address[1]);
+            $("#org-unit").val(orgUnit);
+            $("#common-name").val(nicId);
+            $("#org").val($("#org-id").val());
         }
-
-        else {
-            orgUnit = roles[0];
-        }
-
-        //$("#country").val(address[2]);
-        //$("#locality").val(address[1]);
-        $("#org-unit").val(orgUnit);
-        $("#common-name").val(nicId);
-        $("#org").val($("#org-id").val());
-    }
-
+    })//remove these after uncommenting code
 
 });
-
-
-
-
-
-
